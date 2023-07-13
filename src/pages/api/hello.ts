a6d6e206b9871013ae7dd61082272f57e3373508
kelvinmitaki <mitakikelvin1@gmail.com>
2023-07-13 23:40:23 +0300

    wired up converting docx to pdf

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,47 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const file = fs.readFileSync(
+    "/Users/kelvinmitaki/Downloads/docxexample.docx"
+  );
+
+  const formData = new FormData();
+
+  const blob = new Blob([file]);
+
+  formData.append("1", blob, "docxexample.docx");
+
+  formData.append(
+    "ConversionOptions",
+    JSON.stringify({
+      MinimizeTheNumberOfWorksheets: "false",
+      UseOcr: "false",
+      Locale: "en",
+      Password: null,
+    })
+  );
+
+  const { data } = await axios.post(
+    "https://api.products.aspose.app/words/conversion/api/convert?outputType=PDF",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+
+  console.log(data);
+
+  res.status(200).json({ name: "John Doe" });
 }
